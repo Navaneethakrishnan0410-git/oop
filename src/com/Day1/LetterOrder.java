13ed0a8ba5576b32944a887b5788c3b3e5a8a47e
Navaneethakrishnan0410-git <navaneethakrishnan0410@gmail.com>
2025-10-30 11:11:18 +0530

    LetterOrder.java

--- src/com/Day1/LetterOrder.java
+++ src/com/Day1/LetterOrder.java
@@ -1,4 +1,14 @@
 package com.Day1;
 
 public class LetterOrder {
+  public static void main(String[] args) {
+        char ch1 = 'z';
+        char ch2 = 'o';
+        
+        if (ch1 < ch2) {
+            System.out.println(ch1 + "," + ch2);
+        } else {
+            System.out.println(ch2 + "," + ch1);
+        }
+    }
 }
