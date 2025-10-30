90abfef73f8766a5b35183f645175d1f7bd5203f
Navaneethakrishnan0410-git <navaneethakrishnan0410@gmail.com>
2025-10-30 11:09:35 +0530

    EvenOddNumber.java

--- src/com/Day1/EvenOddNumber.java
+++ src/com/Day1/EvenOddNumber.java
@@ -1,4 +1,18 @@
 package com.Day1;
-
+import java.util.Scanner;
 public class EvenOddNumber {
+  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter an integer: ");
+        int num = sc.nextInt();
+        
+        if (num % 2 == 0) {
+            System.out.println("The number is Even");
+        } else {
+            System.out.println("The number is Odd");
+        }
+        
+        sc.close();
+    }
 }
