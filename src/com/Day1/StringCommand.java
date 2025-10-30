2d31ed8db0258a058d2c4eb98b551d670f79d2fd
Navaneethakrishnan0410-git <navaneethakrishnan0410@gmail.com>
2025-10-30 11:14:38 +0530

    StringCommand.java

--- src/com/Day1/StringCommand.java
+++ src/com/Day1/StringCommand.java
@@ -1,4 +1,9 @@
 package com.Day1;
 
 public class StringCommand {
+  public static void main(String[] args){
+    System.out.println(args[0]);
+    System.out.println(args[1]);
+    System.out.println(args[0]+ " Technoloies "+ args[1]);
+  }
 }
